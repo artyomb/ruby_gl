337f1a4bb3310af5b598e09e9b821cabfdf5aa76
Your Name <you@example.com>
2018-04-13 11:29:46 +0300

    update

--- bezier.md
+++ bezier.md
@@ -10,20 +10,20 @@ end
 class Array
   def lerp(t); first.lerp(t) * (1.0 - t) + last.lerp(t) * t  end
   
-  def to_bi_tree
+  def to_bezier
     size == 2 ? self :
     each_index.map do |index|
       [self[index], self[index + 1]]
-    end[0...-1].to_bi_tree
+    end[0...-1].to_bezier
   end
 end
 
-p [0, 1].to_bi_tree # [0, 1]
-p [0, 1, 2].to_bi_tree # [[0, 1], [1, 2]]
-p [0, 1, 2, 3].to_bi_tree # [[[0, 1], [1, 2]], [[1, 2], [2, 3]]]
+p [0, 1].to_bezier # [0, 1]
+p [0, 1, 2].to_bezier # [[0, 1], [1, 2]]
+p [0, 1, 2, 3].to_bezier # [[[0, 1], [1, 2]], [[1, 2], [2, 3]]]
 
 points = [Vector[10, 10], Vector[210, 10], Vector[200, 250], Vector[400, 10]]
-bezier_curve = points.to_bi_tree
+bezier = points.to_bezier
 
-path = Array.new(steps) { |index| bezier_curve.lerp(index.to_f / (steps - 1)) }
+path = Array.new(steps) { |index| bezier.lerp(index.to_f / (steps - 1)) }
 ```
--- bezier.rb
+++ bezier.rb
@@ -11,11 +11,11 @@ class Array
     first.lerp(t) * (1.0 - t) + last.lerp(t) * t
   end
 
-  def to_bi_tree
+  def to_bezier
     size == 2 ? self :
     each_index.map do |index|
       [self[index], self[index + 1]]
-    end[0...-1].to_bi_tree
+    end[0...-1].to_bezier
   end
 end
 
@@ -23,15 +23,15 @@ def make_lerp_path(tuple, steps)
   Array.new(steps) { |index| tuple.lerp(index.to_f / (steps - 1)) }
 end
 
-p [0, 1].to_bi_tree # [0, 1]
-p [0, 1, 2].to_bi_tree # [[0, 1], [1, 2]]
-p [0, 1, 2, 3].to_bi_tree # [[[0, 1], [1, 2]], [[1, 2], [2, 3]]]
+p [0, 1].to_bezier # [0, 1]
+p [0, 1, 2].to_bezier # [[0, 1], [1, 2]]
+p [0, 1, 2, 3].to_bezier # [[[0, 1], [1, 2]], [[1, 2], [2, 3]]]
 
 objects = []
-# https://stackoverflow.com/questions/6711707/draw-a-quadratic-b%C3%A9zier-curve-through-three-given-points
+# through points https://stackoverflow.com/questions/6711707/draw-a-quadratic-b%C3%A9zier-curve-through-three-given-points
 # connect curves https://www.scratchapixel.com/lessons/advanced-rendering/bezier-curve-rendering-utah-teapot
 points = [Vector[10, 10], Vector[210, 50], Vector[150, 250], Vector[400, 10]]
-bezier = points.to_bi_tree
+bezier = points.to_bezier
 
 objects << Render.grey(points)
 objects += points.map { |p| Render.white p }
@@ -39,16 +39,15 @@ objects += points.map { |p| Render.white p }
 curve = make_lerp_path(bezier, 30)
 objects << Render.green(curve)
 
-# r_proc = Render.proc do |o|
-#   color red
-#   point o.position
-#   path o.path
-# end
-
-
 renderer = Render.new
 renderer.scene = { objects: objects, types: {
   Vector => { circle: ->(v) { v } }, Array => { path: ->(a) { a } }
 } }
+
+# renderer.scene[:types][MyObject] = proc do |r, o|
+#  r.color :red
+#  r.point o.position
+#  r.path o.path
+# end
 
 renderer.run
