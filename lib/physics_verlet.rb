0caf07c1eebdf3a4d514d30c129d31c9eb1a86fb
Your Name <artem.borodkin@gmail.com>
2018-06-03 20:19:22 +0300

    update

---
+++ lib/physics_verlet.rb
@@ -0,0 +1,66 @@
+require 'matrix'
+
+class PhysicsVerlet
+  # second / metre / kilogram
+  # Force Newton (N) = kg⋅m⋅s−2
+  # Joule(J) = N⋅m = kg⋅m2⋅s−2
+  # Watt = J/s = kg⋅m2⋅s−3
+  STEP_TIME = 0.0005 # sec
+  VECTOR_ZERO = Vector[0, 0].freeze
+
+  module PhysicsForce; end
+  module PhysicsConstrain; end
+
+  module PhysicsObject
+    attr_accessor :position, :position_prev, :mass
+    attr_accessor :force_sum, :force
+    def sim_params(position:, velocity: VECTOR_ZERO, mass: 1.0)
+      @position = position
+      @position_prev = position - velocity * STEP_TIME * 0.0
+      @mass = mass
+      @force_sum = VECTOR_ZERO
+      @force = VECTOR_ZERO
+    end
+
+    def velocity
+      (@position - @position_prev) / STEP_TIME
+    end
+  end
+
+  attr_accessor :forces
+  def initialize
+    @forces = []
+  end
+
+  def step(objects, delta = STEP_TIME)
+    @physics_objects ||= objects.select { |o| o.class.ancestors.include? PhysicsObject }
+    @physics_forces ||= objects.select { |o| o.class.ancestors.include? PhysicsForce }
+    @physics_constrains ||= objects.select { |o| o.class.ancestors.include? PhysicsConstrain }
+
+    @physics_forces.each(&:step)
+
+    # TODO unreal: http://www.aclockworkberry.com/unreal-engine-substepping/
+    #
+    # Verlet integration
+    # TODO http://lonesock.net/article/verlet.html
+    # xi+1 = xi + (xi - xi-1) * (dti / dti-1) + a * dti * dti
+    # http://davidlively.com/programming/simple-physics-fun-with-verlet-integration/
+    @physics_objects.each do |obj|
+      # timeScale = elapsedSeconds / previousFrameTime
+      # velocity = (currentTranslation – previousTranslation) * timeScale
+
+      @forces.each { |force| obj.force_sum += force.call(obj, []) }
+
+      prev = obj.position
+      df = obj.mass > 0 ? obj.force_sum * delta * delta / obj.mass : VECTOR_ZERO
+      obj.position += obj.position - obj.position_prev + df
+      obj.position_prev = prev
+
+      obj.force = obj.force_sum
+      obj.force_sum = VECTOR_ZERO
+    end
+
+    # Relaxation / Jacobi / Gauss-Seidel
+    1.times { @physics_constrains.each(&:step) }
+  end
+end
--- simulation4.rb
+++ simulation4.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 # https://vectr.com/tmp/b1vPZfoE0b/a1Q2d7s6e
 require './lib/render'
-require './lib/physics'
-$stdout.sync = false
+require './lib/physics_verlet'
+$stdout.sync = true
 
 class Array
   def each_with_each
@@ -11,6 +11,13 @@ class Array
         next if i2 <= i
         next if e == e2
         yield e, e2
+      end
+    end
+  end
+  def each_with_each_full
+    each do |e|
+      each do |e2|
+        yield e, e2 unless e == e2
       end
     end
   end
@@ -38,46 +45,66 @@ class Scene
 end
 
 class Node
-  include Physics::PhysicsObject
+  include PhysicsVerlet::PhysicsObject
   def initialize(position:, velocity: Vector[0, 0], mass: 1.0)
     sim_params(position: position, velocity: velocity, mass: mass)
   end
 end
 
-class Link
-  include Physics::PhysicsForce
-  PRIORITY = 1
-  attr_accessor :list
-  def initialize(array)
-    @list = array
-    @distances = {}
-    @list.each_with_each do |o, o2|
-      @distances["#{o.object_id}-#{o2.object_id}"] = (o2.position - o.position).magnitude
-    end
+class HardLink
+  include PhysicsVerlet::PhysicsConstrain
+  attr_accessor :pair
+  def initialize(o1, o2)
+    @pair = [o1, o2]
+    @distance = (o1.position - o2.position).magnitude
   end
-end
 
-class SoftLink < Link
   def step
-    @list.each{|o|
-      o.forces = o.force_list.dup
-    }
-
-    @list.each_with_each do |o, o2|
-      original = @distances["#{o.object_id}-#{o2.object_id}"]
-      r = o2.position - o.position
-      delta = r.magnitude - original
-      delta *= 0.002
-      next unless delta != 0
-
-      o.force_list << r.normalize * delta
-      o2.force_list << -r.normalize * delta
+    o1, o2 = @pair
+    r = o2.position - o1.position
+    delta = r.magnitude - @distance
+    delta *= 0.6
+    return unless delta != 0
+    if o2.mass > 0
+      k = o1.mass / o2.mass
+      d1 = delta * k / (1 + k)
+      d2 = delta - d1
+      o1.position += r.normalize * d2
+      o2.position -= r.normalize * d1
+    else
+      o2.position -= r.normalize * delta
     end
   end
 end
+
+class SoftLink
+  include PhysicsVerlet::PhysicsForce
+  attr_accessor :pair, :k
+  def initialize(o1, o2, k = 300)
+    @k = k
+    @pair = [o1, o2]
+    @distance = (o1.position - o2.position).magnitude
+  end
+
+  def displacement
+    r = @pair.first.position - @pair.last.position
+    r.magnitude - @distance
+  end
+
+  def step
+    o1, o2 = @pair
+    r = o1.position - o2.position
+    rn = r.normalize
+    delta = r.magnitude - @distance
+    delta += 0.1 * (o1.velocity - o2.velocity).dot(rn)
+    o1.force_sum += -rn * delta * @k
+    o2.force_sum += rn * delta * @k
+  end
+end
+
 
 class Engine
-  include Physics::PhysicsForce
+  include PhysicsVerlet::PhysicsForce
   PRIORITY = 0
   def initialize(node:, power: 1.0, dynamics: 1.0)
     @node = node
@@ -85,7 +112,7 @@ class Engine
     @dynamics = dynamics
   end
   def step
-    @node.force_list << Vector[0, -0.0006]
+    @node.force_sum += Vector[0, -500]
   end
 end
 
@@ -93,26 +120,39 @@ class Ship
   def children(&block)
     return enum_for(:children) unless block_given?
     @engines.each(&block)
-    @links.list.each(&block)
-    yield @links
+    @nodes.each(&block)
+    @links.each(&block)
   end
 
-  def initialize(position:, velocity: Vector[0, 0], size: 200, mass: 1.0)
-    @links = SoftLink.new [
+  def initialize(position:, velocity: Vector[0, 0], size: 200, mass: 100.0)
+    @nodes = [
       Node.new(position: position - Vector[size * 0.5, 0], velocity: velocity, mass: mass * 0.3),
-      Node.new(position: position + Vector[-size * 0.1, -size * 0.4], velocity: velocity, mass: mass * 0.25),
-      # Node.new(position: position + Vector[0, -size * 0.1], velocity: velocity, mass: 0.06),
-      Node.new(position: position + Vector[size * 0.1, -size * 0.4], velocity: velocity, mass: mass * 0.25),
-      Node.new(position: position + Vector[size * 0.5, 0], velocity: velocity, mass: mass * 0.2)
+      Node.new(position: position + Vector[-size * 0.3, -size * 0.4], velocity: velocity, mass: mass * 0.35),
+      Node.new(position: position + Vector[size * 0.3, -size * 0.4], velocity: velocity, mass: mass * 0.35),
+      Node.new(position: position + Vector[size * 0.5, 0], velocity: velocity, mass: mass * 0.3),
+
+      Node.new(position: position + Vector[0, size * 0.05], velocity: velocity, mass: 0),
+      Node.new(position: position + Vector[-size * 0.8, size * 0.3], velocity: velocity, mass: mass * 0.01),
+      Node.new(position: position + Vector[+size * 0.8, size * 0.3], velocity: velocity, mass: mass * 0.01)
     ]
+    @links = [
+      [0, 1], [1, 2], [2, 3], [3, 0], [0, 2], [1, 3], [0, 4], [4, 3], [1, 4], [2,4]
+    ].map{ |pair| HardLink.new *pair.map{ |i| @nodes[i] } }
+
+    @links << HardLink.new(@nodes[0], @nodes[5])
+    @links << SoftLink.new(@nodes[4], @nodes[5])
+
+    @links << HardLink.new(@nodes[3], @nodes[6])
+    @links << SoftLink.new(@nodes[4], @nodes[6])
+
     @engines = []
-    @engines << Engine.new(node: @links.list.first)
-    @engines << Engine.new(node: @links.list.last)
+    @engines << Engine.new(node: @nodes[0])
+    @engines << Engine.new(node: @nodes[3])
   end
 end
 
 class CollisionDetection
-  include Physics::PhysicsForce
+  include PhysicsVerlet::PhysicsForce
   PRIORITY = 100
   def initialize(objects)
     @objects = objects
@@ -120,9 +160,10 @@ class CollisionDetection
 
   def step
     @objects.each do |o|
-      if o.position[1] > 400 && o.velocity[1] > -0.001
+      if o.position[1] > 400 # && o.velocity[1] > -0.01
         depth = o.position[1] - 400
-        o.force_list << Vector[0, -0.0003 * depth]
+        o.force_sum += Vector[0, -200 * depth]
+        # - bn(n.v)
       end
     end
   end
@@ -135,27 +176,58 @@ renderer = Render.new title: 'Ship'
 renderer.scene = { objects: world,
                    types: {
                      Node => proc { |r, o|
-                           r.circle o.position
+                           r.circle o.position, o.mass * 0.2
                            glColor3fv [1.0, 0.5, 0.5]
-                           r.path [o.position, o.position + o.forces.inject(&:+) * 100000] rescue nil
+                           r.path [o.position, o.position + o.force * 0.1]
                          },
-                     SoftLink => { path: ->(a) { a.list.map(&:position) << a.list.first.position } },
+                     HardLink => { path: ->(a) { a.pair.map(&:position) } },
+                     SoftLink => proc { |r, o|
+                       glColor3fv [1 - o.displacement * 0.01, 0, 1 + o.displacement * 0.01]
+                       r.path o.pair.map(&:position)
+                     },
                    }
 }
 
-physics = Physics.new
-world << CollisionDetection.new(world.select { |o| o.class.ancestors.include? Physics::PhysicsObject })
+physics = PhysicsVerlet.new
+world << CollisionDetection.new(world.select { |o| o.class.ancestors.include? PhysicsVerlet::PhysicsObject })
 
 # Gravity
 physics.forces << proc do |obj|
-  0.002 * obj.mass * Vector[0, 1]
-end
-# Air
-physics.forces << proc do |obj|
-  0.001 * (obj.velocity.magnitude**2) * -obj.velocity.normalize rescue Vector[0,0]
+  9.81 * obj.mass * Vector[0, 1]
 end
 
-renderer.run do
-  time = Time.now.to_f * 25
-  physics.step world, time
+class FPS
+  def initialize(title)
+    @title = title
+    @p_time = Time.new.to_f
+    @list = Array.new 100, 0
+    @last = Time.now.to_f
+  end
+
+  def ping
+    @list.push (Time.new.to_f - @last).to_f
+    @last = Time.now.to_f
+    @list.shift
+  end
+
+  def print
+    ping
+    if Time.new.to_f - @p_time > 2
+      avg = @list.inject(&:+) / @list.size
+      puts "#{@title} FPS: #{1 / avg}"
+      @p_time = Time.new.to_f
+    end
+  end
+
 end
+
+p_fps = FPS.new :Physics
+fps = FPS.new :Render
+renderer.run do
+  100.times do
+    physics.step world
+    p_fps.print
+  end
+  fps.print
+end
+
